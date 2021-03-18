5a9c130f8e1396d1118097da33d9737d0aaf0e6d
dri14maretti <dri.cmaretti@gmail.com>
2021-03-18 18:22:51 -0300

    app base

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Student } from './student.model';
 
 @Component({
-  selector: 'jad-student',
-  templateUrl: './student.component.html',
-  styleUrls: ['./student.component.css']
+	selector: 'jad-student',
+	templateUrl: './student.component.html',
+	styleUrls: ['./student.component.css'],
 })
 export class StudentComponent implements OnInit {
+	@Input() student: Student = { name: '', isJedi: false };
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {}
 
+	clicked() {
+		console.log(`Student: ${this.student.name}`);
+	}
 }
